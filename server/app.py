c1fd9132a2597ffba7d7642ee40838edf3e0d024
Waithera <stacywaithera@DESKTOP-E0D09HN.localdomain>
2025-06-19 00:13:45 +0300

    restful crud lab

--- server/app.py
+++ server/app.py
@@ -47,6 +47,29 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return {"error": "Plant not found"}, 404
+
+        data = request.get_json()
+
+        for attr in data:
+            if hasattr(plant, attr):
+                setattr(plant, attr, data[attr])
+
+        db.session.commit()
+        return plant.to_dict(), 200
+
+    def delete(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return {"error": "Plant not found"}, 404
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return "", 204
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
